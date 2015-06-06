0bafc48521080f4955a4170b0c9b257d4f6ae714
pko104 <peterrko1004@gmail.com>
2015-06-06 01:30:25 -0400

    added modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require foundation
 //= require_tree .
+//= require foundation
 
-
-$(function(){ $(document).foundation(); });
--- db/migrate/20150504213135_create_private_messages.rb
+++ db/migrate/20150504213135_create_private_messages.rb
@@ -1,6 +1,8 @@
 class CreatePrivateMessages < ActiveRecord::Migration
   def change
     create_table :private_messages do |t|
+      t.string :description, null: false
+      t.integer :user_id, null: false
     end
   end
 end
