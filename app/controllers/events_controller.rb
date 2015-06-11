bee53525ea497b1cc438b6e9e095f0d9841fa33c
pko104 <peterrko1004@gmail.com>
2015-06-11 01:01:42 -0400

    fixed form save

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,18 @@
 class EventsController < ApplicationController
   def create
-    @new_event.save
+    @new_event = Event.new(event_params)
+    if @new_event.save
+      redirect_to @new_event, notice: "new_event"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :location, :date, :time)
   end
 end
