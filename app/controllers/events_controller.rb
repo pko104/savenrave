71d6cb379ff37028434983710a30c0df52b06ae9
pko104 <peterrko1004@gmail.com>
2015-06-18 23:22:49 -0400

    network formuals

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,6 +12,41 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def network
+    @user = User.find(params[:id])
+    @current_user = current_user
+    if @current_user.network_id == nil
+      @network = Network.new(user_id: @current_user.id)
+      @network.connected_ids_array = ""
+      @network.save
+      @current_user.network_id = @network.id
+      @current_user.save
+      @messages = Message.where(user_id: @user)
+    else
+      @newsfeed = []
+      @networks = Network.where(user_id: @user)
+      if @networks.length >= 1 && @networks != nil
+        @networks.each do |network|
+          network.messages.each do |nw|
+            @newsfeed << nw
+          end
+          if network.connected_ids_array != nil && network.connected_ids_array.length >= 1
+          friends = network.connected_ids_array.split(',')
+            friends.each do |friend|
+              @friend_network = Network.where(user_id: friend)
+              if @friend_network.length >= 1 && @friend_network != nil
+                @friend_network.each do |f|
+                  f.messages.each do |p|
+                    @newsfeed << p
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+
   def event_params
     params.require(:event).permit(:name, :location, :date, :time)
   end
