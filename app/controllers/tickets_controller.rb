2ebc20f626d0e60df679dfbd123194f5f38c0956
pko104 <peterrko1004@gmail.com>
2015-07-13 22:56:29 -0400

    working ticket modal

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,26 +1,22 @@
 class TicketsController < ApplicationController
 
   def create
-    @event = Event.find(params[:id])
     @new_ticket = Ticket.new(ticket_params)
-    @new_ticket.event_id == @event.id
-
-    if current_user.id
-      @new_ticket.user_id == current_user.id
-    else
-      @new_ticket.user_id == 1
-    end
+ #   if current_user.id
+ #     @new_ticket.user_id == current_user.id
+ #   else
+      @new_ticket.user_id = 1
+ #   end
 
     if @new_ticket.save
-      redirect_to @event, notice: "new_event"
+      redirect_to @new_ticket.event, notice: "new_event"
     else
       render :new
     end
   end
 
   def ticket_params
-    params.require(:ticket).permit(:postdate, :enddate, :price)
+    params.require(:ticket).permit(:postdate, :enddate, :price, :event_id)
   end
-
 
 end
